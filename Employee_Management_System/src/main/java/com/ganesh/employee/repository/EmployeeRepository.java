1a1fae53009a29d2c69edfc741599dc4ca406d1c
ganesh manish gopale <57770556+ganesh208@users.noreply.github.com>
2024-01-04 21:44:47 +0530

    final code

---
+++ Employee_Management_System/src/main/java/com/ganesh/employee/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.ganesh.employee.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.ganesh.employee.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
